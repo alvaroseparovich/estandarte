21a423044bcfa6fcd4b436d725f4db7cb6a71c35
Alvaro Separovich <alvasepro@gmail.com>
2019-04-10 20:43:40 -0300

    -CHANGE: Font-Famaly changed

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function featured_news_enqueue_child_styles() {
     $min = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
     $parent_style = 'magazine-7-style';
 
-    $fonts_url = 'https://fonts.googleapis.com/css?family=EB+Garamond|Fira+Sans+Condensed';
+    $fonts_url = 'https://fonts.googleapis.com/css?family=EB+Garamond|Fira+Sans+Condensed|Merriweather';
     wp_enqueue_style('featured-news-google-fonts', $fonts_url, array(), null);
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap' . $min . '.css');
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css' );
@@ -43,3 +43,48 @@ function bbloomer_price_free_zero_empty( $price, $product ){
     { $price = '';} 
     return $price;
 }
+
+
+//---------------------------------------------------------
+function human_time_diff_chinese( $from, $to = '' ) {
+    if ( empty( $to ) ) {
+      $to = time();
+    }
+  
+    $diff = (int) abs( $to - $from );
+  
+    if ( $diff < HOUR_IN_SECONDS ) {
+      $mins = round( $diff / MINUTE_IN_SECONDS );
+      if ( $mins <= 1 )
+        $mins = 1;
+      /* translators: min=minute */
+      $since = sprintf( _n( '%s min', '%s minutos', $mins ), $mins );
+    } elseif ( $diff < DAY_IN_SECONDS && $diff >= HOUR_IN_SECONDS ) {
+        $hours = round( $diff / HOUR_IN_SECONDS );
+        if ( $hours <= 1 )
+            $hours = 1;
+        $since = sprintf( _n( '%s hora', '%s horas', $hours ), $hours );
+    } elseif ( $diff < WEEK_IN_SECONDS && $diff >= DAY_IN_SECONDS ) {
+        $days = round( $diff / DAY_IN_SECONDS );
+        if ( $days <= 1 )
+            $days = 1;
+        $since = sprintf( _n( '%s dia', '%s dias', $days ), $days );
+    } elseif ( $diff < MONTH_IN_SECONDS && $diff >= WEEK_IN_SECONDS ) {
+        $weeks = round( $diff / WEEK_IN_SECONDS );
+        if ( $weeks <= 1 )
+            $weeks = 1;
+        $since = sprintf( _n( '%s semana', '%s semanas', $weeks ), $weeks );
+    } elseif ( $diff < YEAR_IN_SECONDS && $diff >= MONTH_IN_SECONDS ) {
+        $months = round( $diff / MONTH_IN_SECONDS );
+        if ( $months <= 1 )
+            $months = 1;
+        $since = sprintf( _n( '%s mes', '%s meses', $months ), $months );
+    } elseif ( $diff >= YEAR_IN_SECONDS ) {
+        $years = round( $diff / YEAR_IN_SECONDS );
+        if ( $years <= 1 )
+            $years = 1;
+        $since = sprintf( _n( '%s ano', '%s anos', $years ), $years );
+    }
+  
+    return apply_filters( 'human_time_diff_chinese', $since, $diff, $from, $to );
+  }

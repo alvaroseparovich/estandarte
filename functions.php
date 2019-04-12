1b221e9fb98cb5394b025f81a1ba3ce220499ae4
Alvaro Separovich <alvasepro@gmail.com>
2019-04-12 19:47:57 -0300

    -FEAT: placeholder on checkout added

--- functions.php
+++ functions.php
@@ -43,3 +43,51 @@ function bbloomer_price_free_zero_empty( $price, $product ){
     { $price = '';} 
     return $price;
 }
+
+
+//checkout com os campos bairro obrigatorio, 5 numeros no numero, e 30 no complemento
+function wc_etc_bfield( $fields ) {
+    $fields['billing_number']['maxlength'] = 5;
+    $fields['billing_address_2']['maxlength'] = 30;
+    $fields['billing_address_2']['label_class'] = array('');    
+    $fields['billing_neighborhood']['required'] = true;
+
+    $fields['billing_first_name']['placeholder'] = 'Primeiro Nome';
+    $fields['billing_last_name']['placeholder'] = 'Sobrenome';
+    $fields['billing_cpf']['placeholder'] = 'CPF';
+    $fields['billing_postcode']['placeholder'] = 'CEP';
+    $fields['billing_address_1']['placeholder'] = 'Nome da Rua';
+    $fields['billing_number']['placeholder'] = 'Numero';
+    $fields['billing_address_2']['placeholder'] = 'Complemento';
+    $fields['billing_neighborhood']['placeholder'] = 'Bairro';
+    $fields['billing_city']['placeholder'] = 'Cidade';
+    $fields['billing_phone']['placeholder'] = 'Telefone';
+    $fields['billing_cellphone']['placeholder'] = 'Celular';
+    $fields['billing_email']['placeholder'] = 'Seu email';
+    $fields['billing_company']['placeholder'] = 'Nome da empresa';
+    $fields['billing_cnpj']['placeholder'] = 'CNPJ';
+
+    return $fields;}
+add_filter( 'woocommerce_billing_fields', 'wc_etc_bfield' );
+function wc_etc_sfield( $fields ) {
+    $fields['shipping_number']['maxlength'] = 5;
+    $fields['shipping_address_2']['maxlength'] = 30;
+    $fields['shipping_address_2']['label_class'] = array('');  
+    $fields['shipping_neighborhood']['required'] = true;
+
+    $fields['shipping_first_name']['placeholder'] = 'Primeiro Nome';
+    $fields['shipping_last_name']['placeholder'] = 'Sobrenome';
+    $fields['shipping_postcode']['placeholder'] = 'CEP';
+    $fields['shipping_address_1']['placeholder'] = 'Nome da Rua';
+    $fields['shipping_number']['placeholder'] = 'Numero';
+    $fields['shipping_address_2']['placeholder'] = 'Complemento';
+    $fields['shipping_neighborhood']['placeholder'] = 'Bairro';
+    $fields['shipping_city']['placeholder'] = 'Cidade';
+    $fields['shipping_company']['placeholder'] = 'Nome da empresa';
+
+    return $fields;}
+add_filter( 'woocommerce_shipping_fields', 'wc_etc_sfield' );
+function wc_etc_ordernote( $fields ) {
+     unset($fields['order']['order_comments']);
+     return $fields;}
+add_filter( 'woocommerce_checkout_fields' , 'wc_etc_ordernote' );
